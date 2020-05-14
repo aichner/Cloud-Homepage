2c54411d1a2058e670addec89662cc7aee27bd91
Luca Allmaier <allmaier@aichner.cloud>
2020-05-14 23:08:33 +0200

    Improve code quality
    The files have been formatted automatically.

--- src/Routes.js
+++ src/Routes.js
@@ -8,16 +8,14 @@ import { Route, Switch } from "react-router-dom";
 /**
  * HomePage: A basic template page
  */
-import {
-  HomePage
-} from "./components/pages";
+import { HomePage } from "./components/pages";
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={HomePage} />
-          
+
         <Route
           render={function () {
             return <h1>Not Found</h1>;
@@ -30,7 +28,7 @@ class Routes extends React.Component {
 
 export default Routes;
 
-/** 
+/**
  * SPDX-License-Identifier: (EUPL-1.2)
- * Copyright © 2019 Werbeagentur Christian Aichner
+ * Copyright © 2019-2020 Werbeagentur Christian Aichner
  */
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,12 +1,11 @@
 //> Components
 // Import all components to export them for easy access from parent components
 import HomePage from "./HomePage";
+import LandingPage from "./LandingPage";
 
-export {
-    HomePage,
-};
+export { HomePage, LandingPage };
 
-/** 
+/**
  * SPDX-License-Identifier: (EUPL-1.2)
- * Copyright © 2019 Werbeagentur Christian Aichner
+ * Copyright © 2019-2020 Werbeagentur Christian Aichner
  */
