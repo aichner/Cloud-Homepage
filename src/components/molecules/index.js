7783922d859a53ce37bec35e16b1969455d75cf1
Luca Allmaier <allmaier@aichner.cloud>
2020-05-15 00:25:55 +0200

    Remove exports
    Exports of deleted files have been removed.

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,9 +1,5 @@
 //> Components
 // Import all components to export them for easy access from parent components
-import Footer from "./Footer";
-import Navbar from "./Navbar";
-
-export { Footer, Navbar };
 
 /**
  * SPDX-License-Identifier: (EUPL-1.2)
